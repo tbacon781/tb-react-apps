c1d88374e8e47b161b0633db3b9569326a129c35
tbacon781 <71884321+tbacon781@users.noreply.github.com>
2024-07-22 11:55:20 -0400

    Add files via upload

---
+++ night-toggle/src/components/toggle.jsx
@@ -0,0 +1,16 @@
+import './toggle.css';
+
+export const Toggle = ({ handleChange, isChecked }) => {
+  return (
+    <div className='toggle-container'>
+      <input
+        type='checkbox'
+        id='check'
+        className='toggle'
+        onChange={handleChange}
+        checked={isChecked}
+      />
+      <label htmlFor='check'></label>
+    </div>
+  );
+};
